44ceb11a04bf5557689313897a46b017607a0700
Gucio1200 <kacper@linuxology.pl>
2021-01-17 02:43:32 +0100

    modified:   Dockerfile 	modified:   docker-entrypoint.sh

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM amazon/aws-cli:latest
 RUN amazon-linux-extras install epel && \
-    yum install -y wget shadow-utils && \
+    yum install -y wget shadow-utils util-linux && \
     yum install -y https://download-ib01.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/f/fswatch-1.14.0-3.el8.x86_64.rpm && \
     wget -O /usr/bin/dumb-init https://github.com/Yelp/dumb-init/releases/download/v1.2.4/dumb-init_1.2.4_x86_64 && \
     chmod +x /usr/bin/dumb-init
@@ -13,7 +13,7 @@ RUN ln -s /usr/local/bin/docker-entrypoint.sh / && \
 
 USER tape
 
-#ENTRYPOINT ["docker-entrypoint.sh"]
+ENTRYPOINT ["docker-entrypoint.sh"]
 
-ENTRYPOINT ["/usr/bin/dumb-init", "--"]
-CMD ["docker-entrypoint.sh"]
+#ENTRYPOINT ["/usr/bin/dumb-init", "--"]
+#CMD ["docker-entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,14 +5,38 @@ TRIGGER_FILE=${TRIGGER_FILE:-.do_upload}
 SYNC_FROM=${SYNC_FROM:-/var/www/html/wp-content/s3}
 PARAMS=${PARAMS:---acl public-read --delete}
 BUCKET=${BUCKET:-zoomcms.htdwork.com}
+FILES_TO_REMOVE=${TRIGGER_FILE},.lock
+TTL=${TTL:-1200}
+
+LOCKFILE=$SYNC_FROM/.lock
 
 while true
 do
-	NOW=$(date +"%m-%d-%Y-%T")
-	if [ -f "$SYNC_FROM/$TRIGGER_FILE" ]; then
-        	/usr/local/bin/aws s3 sync ${SYNC_FROM} s3://${BUCKET}/ ${PARAMS}
-                /usr/local/bin/aws s3 rm s3://${BUCKET}/${TRIGGER_FILE}
-	echo "Syncing & Cleaning deploy file... $NOW"
-        rm ${SYNC_FROM}/${TRIGGER_FILE}
-	fi
+
+if [ -f "$SYNC_FROM/$TRIGGER_FILE" ]; then
+
+        NOW=$(date +"%m-%d-%Y-%T")
+
+        if (set -o noclobber; echo "$$" > "$LOCKFILE") 2> /dev/null;
+        then
+                trap 'rm -f "$LOCKFILE"; exit $?' INT TERM EXIT
+
+                echo "Syncing & Cleaning deploy file... $NOW"
+                /usr/local/bin/aws s3 sync ${SYNC_FROM} s3://${BUCKET}/ ${PARAMS}
+                for file in $(echo $FILES_TO_REMOVE | sed "s/,/ /g")
+                do
+                        /usr/local/bin/aws s3 rm s3://${BUCKET}/$file
+                done
+                rm -f "$LOCKFILE"
+                rm ${SYNC_FROM}/${TRIGGER_FILE}
+                trap - INT TERM EXIT
+        else
+                echo "Failed to acquire lock-file: $LOCKFILE. waiting..."
+		if [ $(($(date +%s)-$(stat -c "%Y" $LOCKFILE))) -gt $TTL ]; then
+			rm -f "$LOCKFILE"
+			echo "Lock file older then ${TTL}s - so removing..."
+        	fi
+		sleep 60        
+        fi
+fi
 done
