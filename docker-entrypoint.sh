e73959f8c95f47238c78d01476ae8d35372a791a
kacpergogol-htd <77334384+kacpergogol-htd@users.noreply.github.com>
2021-01-27 19:45:31 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,9 +3,10 @@
 
 TRIGGER_FILE=${TRIGGER_FILE:-.do_upload}
 SYNC_FROM=${SYNC_FROM:-/var/www/html/wp-content/s3}
-PARAMS=${PARAMS:---acl public-read}
+PARAMS=${PARAMS:---acl public-read --only-show-errors --delete --exclude "media/*"}
 BUCKET=${BUCKET:-zoomcms.htdwork.com}
-FILES_TO_REMOVE=${TRIGGER_FILE},.lock
+OMMIT=".lock,.sync_log,.do_upload"
+OMMIT_FILES=$(echo $(for file in $(echo $OMMIT | sed "s/,/ /g"); do echo -n " --exclude" $file; done))
 TTL=${TTL:-1200}
 
 LOCKFILE=$SYNC_FROM/.lock
@@ -21,15 +22,8 @@ if [ -f "$SYNC_FROM/$TRIGGER_FILE" ]; then
         if (set -o noclobber; echo "$$" > "$LOCKFILE") 2> /dev/null;
         then
                 trap 'rm -f "$LOCKFILE"; exit $?' INT TERM EXIT
-
                 echo "Syncing & Cleaning deploy file... $NOW"
-                /usr/local/bin/aws s3 sync ${SYNC_FROM} s3://${BUCKET}/ ${PARAMS}
-                for file in $(echo $FILES_TO_REMOVE | sed "s/,/ /g")
-                do
-                        /usr/local/bin/aws s3 rm s3://${BUCKET}/$file
-                done
-                rm -f "$LOCKFILE"
-                rm ${SYNC_FROM}/${TRIGGER_FILE}
+                /usr/local/bin/aws s3 sync ${SYNC_FROM} s3://${BUCKET}/ ${OMMIT_FILES} ${PARAMS} && find ${SYNC_FROM} -mindepth 1 -delete
                 trap - INT TERM EXIT
         else
                 echo "Failed to acquire lock-file: $LOCKFILE. waiting..."
@@ -37,7 +31,7 @@ if [ -f "$SYNC_FROM/$TRIGGER_FILE" ]; then
 			rm -f "$LOCKFILE"
 			echo "Lock file older then ${TTL}s - so removing..."
         	fi
-		sleep 60        
+		sleep 60
         fi
 fi
 done
