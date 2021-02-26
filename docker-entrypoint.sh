56031f9307b3cadd140a359f190cde28cedfe917
kacpergogol-htd <77334384+kacpergogol-htd@users.noreply.github.com>
2021-02-26 14:17:44 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,9 +3,10 @@
 
 TRIGGER_FILE=${TRIGGER_FILE:-.do_upload}
 SYNC_FROM=${SYNC_FROM:-/var/www/html/wp-content/s3}
-PARAMS=${PARAMS:---acl public-read --only-show-errors --delete --exclude "media/*"}
+PARAMS=${PARAMS:---only-show-errors}
+PARAMS_FULL=${PARAMS_CLEAN:-${PARAMS} --delete --exclude "media/*"}
 BUCKET=${BUCKET:-zoomcms.htdwork.com}
-OMMIT=".lock,.sync_log,.do_upload"
+OMMIT=".lock,.sync_log,.do_upload,.do_upload_single"
 OMMIT_FILES=$(echo $(for file in $(echo $OMMIT | sed "s/,/ /g"); do echo -n " --exclude" $file; done))
 TTL=${TTL:-1200}
 
@@ -13,17 +14,20 @@ LOCKFILE=$SYNC_FROM/.lock
 
 while true
 do
-sleep 0.1
+sleep 1
 
-if [ -f "$SYNC_FROM/$TRIGGER_FILE" ]; then
-
+if [ $(find $SYNC_FROM -type f -name $TRIGGER_FILE* | wc -l) -gt 0 ]; then
+        file=$(find $SYNC_FROM -type f -name $TRIGGER_FILE* -exec basename {} \;)
+	echo "Got... $file"
         NOW=$(date +"%m-%d-%Y-%T")
 
         if (set -o noclobber; echo "$$" > "$LOCKFILE") 2> /dev/null;
         then
                 trap 'rm -f "$LOCKFILE"; exit $?' INT TERM EXIT
                 echo "Syncing & Cleaning deploy file... $NOW"
-                /usr/local/bin/aws s3 sync ${SYNC_FROM} s3://${BUCKET}/ ${OMMIT_FILES} ${PARAMS} && find ${SYNC_FROM} -mindepth 1 -delete
+                echo "Running S3 Sync.." && /usr/local/bin/aws s3 sync ${SYNC_FROM} s3://${BUCKET}/ ${OMMIT_FILES} $(echo $(if [[ $file = ${TRIGGER_FILE}_single ]]; then echo ${PARAMS}; else echo ${PARAMS_FULL};fi)) \
+		&& echo "Deleting files.." && find ${SYNC_FROM} -mindepth 1 -delete \
+                && echo "Running Invalidation on ${DISTRIBUTION_ID}.." && /usr/local/bin/aws cloudfront create-invalidation --distribution-id ${DISTRIBUTION_ID} --paths "/*"
                 trap - INT TERM EXIT
         else
                 echo "Failed to acquire lock-file: $LOCKFILE. waiting..."
